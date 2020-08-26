7a4669c3bfeadca4dc0c9a328377c502a0a6637c
Felipe Augusto Pereira de Figueiredo <felipe.figueiredo@inatel.br>
2020-08-26 09:44:33 -0300

    new scripts

--- DetNet.py
+++ DetNet.py
@@ -62,64 +62,64 @@ e.g. training over gaussian i.i.d. channels and testing over a specific constant
 currently both test and train are over i.i.d gaussian channel.
 """
 def generate_data_iid_test(B,K,N,snr_low,snr_high):
-    H_=np.random.randn(B,N,K)
-    W_=np.zeros([B,K,K])
-    x_=np.sign(np.random.rand(B,K)-0.5)
-    y_=np.zeros([B,N])
-    w=np.random.randn(B,N)
-    Hy_=x_*0
-    HH_=np.zeros([B,K,K])
-    SNR_= np.zeros([B])
-    for i in range(B):
-	SNR = np.random.uniform(low=snr_low,high=snr_high)
-        H=H_[i,:,:]
-        tmp_snr=(H.T.dot(H)).trace()/K
-        H_[i,:,:]=H
-        y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
-        Hy_[i,:]=H.T.dot(y_[i,:])
-        HH_[i,:,:]=H.T.dot( H_[i,:,:])
-	SNR_[i] = SNR
-    return y_,H_,Hy_,HH_,x_,SNR_
+   H_=np.random.randn(B,N,K)
+   W_=np.zeros([B,K,K])
+   x_=np.sign(np.random.rand(B,K)-0.5)
+   y_=np.zeros([B,N])
+   w=np.random.randn(B,N)
+   Hy_=x_*0
+   HH_=np.zeros([B,K,K])
+   SNR_= np.zeros([B])
+   for i in range(B):
+      SNR = np.random.uniform(low=snr_low,high=snr_high)
+      H=H_[i,:,:]
+      tmp_snr=(H.T.dot(H)).trace()/K
+      H_[i,:,:]=H
+      y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
+      Hy_[i,:]=H.T.dot(y_[i,:])
+      HH_[i,:,:]=H.T.dot( H_[i,:,:])
+      SNR_[i] = SNR
+   return y_,H_,Hy_,HH_,x_,SNR_
 
 def generate_data_train(B,K,N,snr_low,snr_high):
-    H_=np.random.randn(B,N,K)
-    W_=np.zeros([B,K,K])
-    x_=np.sign(np.random.rand(B,K)-0.5)
-    y_=np.zeros([B,N])
-    w=np.random.randn(B,N)
-    Hy_=x_*0
-    HH_=np.zeros([B,K,K])
-    SNR_= np.zeros([B])
-    for i in range(B):
-	SNR = np.random.uniform(low=snr_low,high=snr_high)
-        H=H_[i,:,:]
-        tmp_snr=(H.T.dot(H)).trace()/K
-        H_[i,:,:]=H
-        y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
-        Hy_[i,:]=H.T.dot(y_[i,:])
-        HH_[i,:,:]=H.T.dot( H_[i,:,:])
-	SNR_[i] = SNR
-    return y_,H_,Hy_,HH_,x_,SNR_
+   H_=np.random.randn(B,N,K)
+   W_=np.zeros([B,K,K])
+   x_=np.sign(np.random.rand(B,K)-0.5)
+   y_=np.zeros([B,N])
+   w=np.random.randn(B,N)
+   Hy_=x_*0
+   HH_=np.zeros([B,K,K])
+   SNR_= np.zeros([B])
+   for i in range(B):
+      SNR = np.random.uniform(low=snr_low,high=snr_high)
+      H=H_[i,:,:]
+      tmp_snr=(H.T.dot(H)).trace()/K
+      H_[i,:,:]=H
+      y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
+      Hy_[i,:]=H.T.dot(y_[i,:])
+      HH_[i,:,:]=H.T.dot( H_[i,:,:])
+      SNR_[i] = SNR
+   return y_,H_,Hy_,HH_,x_,SNR_
 
 
 def piecewise_linear_soft_sign(x):
-    t = tf.Variable(0.1)
-    y = -1+tf.nn.relu(x+t)/(tf.abs(t)+0.00001)-tf.nn.relu(x-t)/(tf.abs(t)+0.00001)
-    return y
+   t = tf.Variable(0.1)
+   y = -1+tf.nn.relu(x+t)/(tf.abs(t)+0.00001)-tf.nn.relu(x-t)/(tf.abs(t)+0.00001)
+   return y
 
 def affine_layer(x,input_size,output_size,Layer_num):
-    W = tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
-    w = tf.Variable(tf.random_normal([1, output_size], stddev=0.01))
-    y = tf.matmul(x, W)+w
-    return y
+   W = tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
+   w = tf.Variable(tf.random_normal([1, output_size], stddev=0.01))
+   y = tf.matmul(x, W)+w
+   return y
 
 def relu_layer(x,input_size,output_size,Layer_num):
-    y = tf.nn.relu(affine_layer(x,input_size,output_size,Layer_num))
-    return y
+   y = tf.nn.relu(affine_layer(x,input_size,output_size,Layer_num))
+   return y
 
 def sign_layer(x,input_size,output_size,Layer_num):
-    y = piecewise_linear_soft_sign(affine_layer(x,input_size,output_size,Layer_num))
-    return y
+   y = piecewise_linear_soft_sign(affine_layer(x,input_size,output_size,Layer_num))
+   return y
 
 #tensorflow placeholders, the input given to the model in order to train and test the network
 HY = tf.placeholder(tf.float32,shape=[None,K])
@@ -144,19 +144,19 @@ BER.append(tf.zeros([]))
 
 #The architecture of DetNet
 for i in range(1,L):
-    temp1 = tf.matmul(tf.expand_dims(S[-1],1),HH)
-    temp1= tf.squeeze(temp1,1)
-    Z = tf.concat([HY,S[-1],temp1,V[-1]],1)
-    ZZ = relu_layer(Z,3*K + v_size , hl_size,'relu'+str(i))
-    S.append(sign_layer(ZZ , hl_size , K,'sign'+str(i)))
-    S[i]=(1-res_alpha)*S[i]+res_alpha*S[i-1]
-    V.append(affine_layer(ZZ , hl_size , v_size,'aff'+str(i)))
-    V[i]=(1-res_alpha)*V[i]+res_alpha*V[i-1]  
-    if LOG_LOSS == 1:
-    	LOSS.append(np.log(i)*tf.reduce_mean(tf.reduce_mean(tf.square(X - S[-1]),1)/tf.reduce_mean(tf.square(X - X_LS),1)))
-    else:
-        LOSS.append(tf.reduce_mean(tf.reduce_mean(tf.square(X - S[-1]),1)/tf.reduce_mean(tf.square(X - X_LS),1)))          
-    BER.append(tf.reduce_mean(tf.cast(tf.not_equal(X,tf.sign(S[-1])), tf.float32)))
+   temp1 = tf.matmul(tf.expand_dims(S[-1],1),HH)
+   temp1= tf.squeeze(temp1,1)
+   Z = tf.concat([HY,S[-1],temp1,V[-1]],1)
+   ZZ = relu_layer(Z,3*K + v_size , hl_size,'relu'+str(i))
+   S.append(sign_layer(ZZ , hl_size , K,'sign'+str(i)))
+   S[i]=(1-res_alpha)*S[i]+res_alpha*S[i-1]
+   V.append(affine_layer(ZZ , hl_size , v_size,'aff'+str(i)))
+   V[i]=(1-res_alpha)*V[i]+res_alpha*V[i-1]  
+   if LOG_LOSS == 1:
+      LOSS.append(np.log(i)*tf.reduce_mean(tf.reduce_mean(tf.square(X - S[-1]),1)/tf.reduce_mean(tf.square(X - X_LS),1)))
+   else:
+      LOSS.append(tf.reduce_mean(tf.reduce_mean(tf.square(X - S[-1]),1)/tf.reduce_mean(tf.square(X - X_LS),1)))
+   BER.append(tf.reduce_mean(tf.cast(tf.not_equal(X,tf.sign(S[-1])), tf.float32)))
 
 TOTAL_LOSS=tf.add_n(LOSS)
 
@@ -169,13 +169,13 @@ init_op=tf.initialize_all_variables()
 sess.run(init_op)
 #Training DetNet
 for i in range(train_iter): #num of train iter
-    batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data_train(train_batch_size,K,N,snr_low,snr_high)
-    train_step.run(feed_dict={HY: batch_HY, HH: batch_HH, X: batch_X})
-    if i % 100 == 0 :
-        batch_Y, batch_H, batch_HY, batch_HH, batch_X ,SNR1= generate_data_iid_test(train_batch_size,K,N,snr_low,snr_high)
-        results = sess.run([loss_LS,LOSS[L-1],ber_LS,BER[L-1]], {HY: batch_HY, HH: batch_HH, X: batch_X})
-        print_string = [i]+results
-        print ' '.join('%s' % x for x in print_string)
+   batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data_train(train_batch_size,K,N,snr_low,snr_high)
+   train_step.run(feed_dict={HY: batch_HY, HH: batch_HH, X: batch_X})
+   if i % 100 == 0 :
+      batch_Y, batch_H, batch_HY, batch_HH, batch_X ,SNR1= generate_data_iid_test(train_batch_size,K,N,snr_low,snr_high)
+      results = sess.run([loss_LS,LOSS[L-1],ber_LS,BER[L-1]], {HY: batch_HY, HH: batch_HH, X: batch_X})
+      print_string = [i]+results
+      print(' '.join('%s' % x for x in print_string))
           
 
 #Testing the trained model
@@ -186,18 +186,18 @@ times = np.zeros((1,num_snr))
 tmp_bers = np.zeros((1,test_iter))
 tmp_times = np.zeros((1,test_iter))
 for j in range(num_snr):
-    for jj in range(test_iter):
-    	print('snr:')
-    	print(snrdb_list[j])
-    	print('test iteration:')
-	print(jj)
-    	batch_Y, batch_H, batch_HY, batch_HH, batch_X ,SNR1= generate_data_iid_test(test_batch_size , K,N,snr_list[j],snr_list[j])
-    	tic = tm.time()
-    	tmp_bers[:,jj] = np.array(sess.run(BER[L-1], {HY: batch_HY, HH: batch_HH, X: batch_X}))    
-    	toc = tm.time()
-	tmp_times[0][jj] =toc - tic
-    bers[0][j] = np.mean(tmp_bers,1)
-    times[0][j] = np.mean(tmp_times[0])/test_batch_size
+   for jj in range(test_iter):
+      print('snr:')
+      print(snrdb_list[j])
+      print('test iteration:')
+      print(jj)
+      batch_Y, batch_H, batch_HY, batch_HH, batch_X ,SNR1= generate_data_iid_test(test_batch_size , K,N,snr_list[j],snr_list[j])
+      tic = tm.time()
+      tmp_bers[:,jj] = np.array(sess.run(BER[L-1], {HY: batch_HY, HH: batch_HH, X: batch_X}))
+      toc = tm.time()
+      tmp_times[0][jj] =toc - tic
+   bers[0][j] = np.mean(tmp_bers,1)
+   times[0][j] = np.mean(tmp_times[0])/test_batch_size
 
 print('snrdb_list')
 print(snrdb_list)
--- FullyConnected.py
+++ FullyConnected.py
@@ -5,44 +5,41 @@ import numpy as np
 import time as tm
 
 
-
 """
 Data generation for train and test phases
 """
 def generate_data(B,K,N,snr_low,snr_high,H_org):
-    #H_=np.random.randn(B,N,K)
-    W_=np.zeros([B,K,K])
-    x_=np.sign(np.random.rand(B,K)-0.5)
-    y_=np.zeros([B,N])
-    w=np.random.randn(B,N)
-    Hy_=x_*0
-    H_ = np.zeros([B,N,K])
-    HH_= np.zeros([B,K,K])
-    SNR_= np.zeros([B])
-    for i in range(B):
-	#print i
-	SNR = np.random.uniform(low=snr_low,high=snr_high)
-        H=H_org
-        tmp_snr=(H.T.dot(H)).trace()/K
-        H_[i,:,:]=H
-        y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
-        Hy_[i,:]=H.T.dot(y_[i,:])
-        HH_[i,:,:]=H.T.dot( H_[i,:,:])
-	SNR_[i] = SNR
-    return y_,H_,Hy_,HH_,x_,SNR_
+   #H_=np.random.randn(B,N,K)
+   W_=np.zeros([B,K,K])
+   x_=np.sign(np.random.rand(B,K)-0.5)
+   y_=np.zeros([B,N])
+   w=np.random.randn(B,N)
+   Hy_=x_*0
+   H_ = np.zeros([B,N,K])
+   HH_= np.zeros([B,K,K])
+   SNR_= np.zeros([B])
+   for i in range(B):
+      #print(i)
+      SNR = np.random.uniform(low=snr_low,high=snr_high)
+      H=H_org
+      tmp_snr=(H.T.dot(H)).trace()/K
+      H_[i,:,:]=H
+      y_[i,:]=(H.dot(x_[i,:])+w[i,:]*np.sqrt(tmp_snr)/np.sqrt(SNR))
+      Hy_[i,:]=H.T.dot(y_[i,:])
+      HH_[i,:,:]=H.T.dot( H_[i,:,:])
+      SNR_[i] = SNR
+   return y_,H_,Hy_,HH_,x_,SNR_
 
 
 def affine_layer(x,input_size,output_size,Layer_num):
-    W = tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
-    w = tf.Variable(tf.random_normal([1, output_size], stddev=0.01))
-    y = tf.matmul(x, W)+w
-    return y
+   W = tf.Variable(tf.random_normal([input_size, output_size], stddev=0.01))
+   w = tf.Variable(tf.random_normal([1, output_size], stddev=0.01))
+   y = tf.matmul(x, W)+w
+   return y
 
 def relu_layer(x,input_size,output_size,Layer_num):
-    y = tf.nn.relu(affine_layer(x,input_size,output_size,Layer_num))
-    return y
-
-
+   y = tf.nn.relu(affine_layer(x,input_size,output_size,Layer_num))
+   return y
 
 """
 simulation - parameters:
@@ -84,12 +81,12 @@ high_snr_test = 10.0 ** (high_snr_db_test/10.0)
 bers = np.zeros((1,num_snr))
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.05)
-    return tf.Variable(initial)
+   initial = tf.truncated_normal(shape, stddev=0.05)
+   return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+   initial = tf.constant(0.1, shape=shape)
+   return tf.Variable(initial)
 
 sess = tf.InteractiveSession()
 
@@ -106,7 +103,7 @@ h_fc.append(tf.nn.relu(tf.matmul(NNinput, W_fc_input) + b_fc_input))
 
 
 for i in range(num_of_hidden_layers):
-    h_fc.append(relu_layer(h_fc[i-1],fc_size, fc_size,'relu'+str(i)))
+   h_fc.append(relu_layer(h_fc[i-1],fc_size, fc_size,'relu'+str(i)))
 
 
 W_fc_final = weight_variable([fc_size, K])
@@ -133,12 +130,12 @@ sess.run(tf.global_variables_initializer())
 training phase og the network
 """
 for i in range(train_iter):
-    batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data(B , K , N , low_snr_train , high_snr_train , H)
-    if i % 100 == 0: 
-        correct_bits = eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
-        train_accuracy = accuracy.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
-        print("step %d, loss is %g, number of correct bits %d" % (i, train_accuracy,correct_bits))
-    train_step.run(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
+   batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data(B , K , N , low_snr_train , high_snr_train , H)
+   if i % 100 == 0:
+      correct_bits = eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
+      train_accuracy = accuracy.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
+      print("step %d, loss is %g, number of correct bits %d" % (i, train_accuracy,correct_bits))
+   train_step.run(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
 
 """
 start testing our net
@@ -151,29 +148,28 @@ testHitCount = 0
 snr_list_db = np.linspace(low_snr_db_test,high_snr_db_test,num_snr)
 snr_list = 10.0**(snr_list_db/10.0)
 for i_snr in range (num_snr):
-    Cur_SNR = snr_list[i_snr]
-    print 'cur snr'
-    print Cur_SNR
-    for i in range(test_iter):
-
-	batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data(B , K , N , Cur_SNR , Cur_SNR , H)
-	tic = tm.time()
-        tmp_bers[0][i] =   eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B}	)
-	toc = tm.time()
-	tmp_times[0][i] = toc - tic
-      	if i % 100 == 0:  
-           	eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
-            	train_accuracy = accuracy.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
-            	print("test accuracy %g" % eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B}))
-    bers[0][i_snr] = np.mean(tmp_bers[0])
+   Cur_SNR = snr_list[i_snr]
+   print('cur snr')
+   print(Cur_SNR)
+   for i in range(test_iter):
+      batch_Y, batch_H, batch_HY, batch_HH, batch_X , SNR1= generate_data(B , K , N , Cur_SNR , Cur_SNR , H)
+      tic = tm.time()
+      tmp_bers[0][i] =   eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B}	)
+      toc = tm.time()
+      tmp_times[0][i] = toc - tic
+      if i % 100 == 0:
+         eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
+         train_accuracy = accuracy.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B})
+         print("test accuracy %g" % eq2.eval(feed_dict={NNinput: batch_Y, org_siganl: batch_X, batchSize: B}))
+   bers[0][i_snr] = np.mean(tmp_bers[0])
     
 times[0][0] = np.mean(tmp_times[0])/B
-print ('Average time to detect a single K bit signal is:')
-print times
+print('Average time to detect a single K bit signal is:')
+print(times)
 bers =  bers/(K*B)
 bers = 1- bers
 snrdb_list = np.linspace(low_snr_db_test,high_snr_db_test,num_snr)
 print('snrdb_list')
 print(snrdb_list)
-print ('Bit error rates are is:')
-print bers
+print('Bit error rates are is:')
+print(bers)
